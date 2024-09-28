2b8e82dcdd4b73b2714a22bcfe97f7ed57a728be
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-09-29 00:10:25 +0530

    Signup Controller Created

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,51 @@
-export const signup = (req, res) => {
-    res.send("Signup");
+import User from "../models/user.model";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+export const signup = async (req, res) => {
+    try {
+        const { name, username, email, password } = req.body;
+
+        const existingEmail = await User.findOne({ email });
+        if (existingEmail) {
+            return res.status(400).json({ message: "Email Already Exists" });
+        }
+
+        const existingUsername = await User.findOne({ username });
+        if (existingUsername) {
+            return res.status(400).json({ message: "Username Already Exists" });
+        }
+
+        if (password.length < 6) {
+            return res.status(400).json({ message: "Password Must be at least 6 Characters" });
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const user = new User({
+            name,
+            email,
+            password: hashedPassword,
+            username
+        });
+        await user.save();
+
+        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: "3d" });
+
+        res.cookie("jwt-linkedin", token, {
+            httpOnly: true,
+            maxAge: 3 * 24 * 60 * 60 * 1000,
+            sameSite: "strict",
+            secure: process.env.NODE_ENV === "production"
+        });
+
+        res.status(201).json({ message: "User Registered Successsfully" });
+    }
+    catch (error) {
+        console.log("Error in Signup: ", error.message);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
 }
 
 export const login = (req, res) => {
