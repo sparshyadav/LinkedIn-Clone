470125dcec5e710e430d197201b6cc72f4dcac44
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-09-28 23:19:16 +0530

    Auth Routes Created

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,12 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 
+import authRoutes from "./routes/auth.route";
+
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use("/api/v1/auth", authRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server Started on Port: ${PORT}`)
