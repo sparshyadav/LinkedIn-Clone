585fa1d61ca75783edb55f59c1412d0dadc93dc2
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-09-28 23:55:12 +0530

    User Model Created

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,8 @@ import { connectDB } from "./lib/db.js";
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json());
+
 app.use("/api/v1/auth", authRoutes);
 
 app.listen(PORT, () => {
