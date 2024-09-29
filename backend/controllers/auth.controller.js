d78456f9b5901663cfe7d51c80308cefb4459a24
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-09-29 11:29:57 +0530

    GetCurrentUser Controller Created

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -93,3 +93,13 @@ export const logout = (req, res) => {
     res.clearCookie("jwt-linkedin");
     res.json({ message: "Logged Out Successfully" });
 }
+
+export const getCurrentUser = async (req, res) => {
+    try {
+        res.json(req.user);
+    }
+    catch (error) {
+        console.error("Error in getCurrentUser Controller: ", error);
+        res.status(500).json({ message: "Server Error" });
+    }
+}
--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { login, logout, signup } from "../controllers/auth.controller.js";
+import { getCurrentUser, login, logout, signup } from "../controllers/auth.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
 const router = express.Router();
 
