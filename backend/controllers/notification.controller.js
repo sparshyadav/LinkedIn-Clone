1786b11697d69ad40171ffdd7968c69e5df24ecf
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-10-07 13:53:59 +0530

    Controller for Marking Notifications as Read Created

--- backend/controllers/notification.controller.js
+++ backend/controllers/notification.controller.js
@@ -14,3 +14,21 @@ export const getUserNotifications = async (req, res) => {
         res.status(500).json({ message: "Internal Server Error" });
     }
 }
+
+export const markNotificationAsRead = async (req, res) => {
+    const notificationId = req.params.id;
+
+    try {
+        const notification = await Notification.findByIdAndUpdate(
+            { _id: notificationId, recipient: req.user._id },
+            { read: true },
+            { new: true }
+        );
+
+        res.status(200).json(notification);
+    }
+    catch (error) {
+        console.error("Error in markNotificationAsRead Controller: ", error);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+}
