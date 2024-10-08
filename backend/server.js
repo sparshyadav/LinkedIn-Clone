84978d77195204bfeb15ae1951c8ae2f3a47df1a
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-10-08 17:13:57 +0530

    Cors Setup Completed

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,24 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-dotenv.config();
-
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
 import postRoutes from "./routes/post.route.js";
 import notificationRoutes from "./routes/notification.route.js";
 import connectionRoutes from "./routes/connection.route.js";
 import { connectDB } from "./lib/db.js";
+import cors from "cors";
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json({ limit: "5mb" }));
 app.use(cookieParser());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}))
 
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/users", userRoutes);
