e68d51022d2d229e9fae3e8dd70262e9f5830711
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-10-08 16:37:09 +0530

    Backend Completed

--- backend/routes/connection.route.js
+++ backend/routes/connection.route.js
@@ -1,6 +1,6 @@
 import express from "express";
-import { protectRoute } from "../middleware/auth.middleware";
-import { sendConnectionRequest } from "../controllers/connection.controller";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { acceptConnectionRequest, getConnectionRequests, getConnectionStatus, getUserConnections, rejectConnectionRequest, removeConnection, sendConnectionRequest } from "../controllers/connection.controller.js";
 const router = express.Router();
 
 router.post("/request/:userId", protectRoute, sendConnectionRequest);
@@ -10,3 +10,5 @@ router.get("/requests", protectRoute, getConnectionRequests);
 router.get("/", protectRoute, getUserConnections);
 router.delete("/:userId", protectRoute, removeConnection);
 router.get("/status/:userId", protectRoute, getConnectionStatus);
+
+export default router;
